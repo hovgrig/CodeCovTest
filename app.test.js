a0cba6aa96753d8f357ecbbc3d1c7bd080921fee
hovgrig <hovgrig@gmail.com>
2024-08-01 15:24:10 +0400

    Not enough coverage test

--- app.test.js
+++ app.test.js
@@ -1,15 +1,10 @@
-// app.test.js
-
-// Import the function to be tested
 const sum = require('./app');
 
 // Describe the test suite
 describe('sum function', () => {
   // Individual test case
   test('should return the sum of two numbers', () => {
-    // Assert that the sum of 1 and 2 is 3
     expect(sum(1, 2)).toBe(3);
-    // Add more test cases if needed
     expect(sum(-1, 1)).toBe(0);
     expect(sum(5, 7)).toBe(12);
   });
